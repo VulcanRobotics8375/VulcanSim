2d1c829026d4f459fbc2be8738cfb9b1bfbda09e
Nico Paoli <39326876+nicobonik@users.noreply.github.com>
2022-01-16 21:39:06 -0800

    add new stuff

--- src/main/java/org/vulcanrobotics/Main.java
+++ src/main/java/org/vulcanrobotics/Main.java
@@ -1,23 +1,66 @@
 package org.vulcanrobotics;
 
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import org.apache.commons.math3.linear.MatrixUtils;
 import org.vulcanrobotics.math.geometry.Pose;
+import org.vulcanrobotics.math.geometry.Vector;
 import org.vulcanrobotics.sim.drivetrains.Mecanum;
 
+import javax.imageio.ImageIO;
+import java.io.FileInputStream;
+
 public class Main extends Application {
+
+    ImageView robotView;
+    int boardDimensions = 800;
+
     @Override
     public void start(Stage stage) throws Exception {
-        stage.setTitle("Hello!");
+        stage.setTitle("VulcanSim dev build 0");
+        var board = new Image(new FileInputStream("src/main/resources/img/ffField.png"));
+        var boardView = new ImageView(board);
+        boardView.setFitHeight(boardDimensions);
+        boardView.setFitWidth(boardDimensions);
+
+        var robot = new Image(new FileInputStream("src/main/resources/img/robot.png"));
+        robotView = new ImageView(robot);
+        robotView.setFitWidth((800.0/144.0) * 12);
+        robotView.setFitHeight((800.0/144.0) * 18);
+
+//        setRobotPosition(robotPositionToPixelPosition(new Pose(72, 72, 0)));
+        robotView.toFront();
+
+        Pane main = new Pane();
+        main.getChildren().add(boardView);
+        main.getChildren().add(robotView);
+        stage.setScene(new Scene(main, boardDimensions, boardDimensions));
         stage.show();
     }
 
-    public static void main(String[] args) {
+    private void setRobotPosition(Pose pose) {
+        robotView.toFront();
+        robotView.setY(pose.y - (robotView.getFitHeight() / 2.0));
+        robotView.setX(pose.x - (robotView.getFitWidth() / 2.0));
+        robotView.setRotate(-Math.toDegrees(pose.heading));
+    }
+
+    private Pose robotPositionToPixelPosition(Pose robotPose) {
+       double pixelsPerInch = boardDimensions / 144.0;
+       return new Pose(robotPose.x * (pixelsPerInch), (robotPose.y * (pixelsPerInch)), robotPose.heading);
+    }
+
+    public static void main(String[] args) throws InterruptedException {
         System.out.println(System.getProperty("os.name"));
         Mecanum mecanum = new Mecanum(4, 4, 1);
         Pose output = mecanum.calculateRobotVelocity(MatrixUtils.createColumnRealMatrix(new double[] {1, -1, 1, -1}));
         System.out.println(output.x + ", " + output.y + ", " + output.heading);
+
         launch();
     }
 }
--- src/main/java/org/vulcanrobotics/ui/FieldController.java
+++ src/main/java/org/vulcanrobotics/ui/FieldController.java
@@ -1,4 +1,20 @@
 package org.vulcanrobotics.ui;
 
-public class FieldController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.image.ImageView;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class FieldController implements Initializable {
+
+    @FXML
+    public ImageView boardView;
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
 }
