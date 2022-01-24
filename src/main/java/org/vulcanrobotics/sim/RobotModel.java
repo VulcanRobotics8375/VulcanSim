d985568a4a62f5514374df11c110a52bdc2e2bcc
Nico Paoli <39326876+nicobonik@users.noreply.github.com>
2022-01-24 13:25:38 -0800

    move pretty much everything from mecanum to robotModel

--- src/main/java/org/vulcanrobotics/sim/RobotModel.java
+++ src/main/java/org/vulcanrobotics/sim/RobotModel.java
@@ -2,7 +2,14 @@ package org.vulcanrobotics.sim;
 
 import org.vulcanrobotics.math.geometry.Pose;
 import org.vulcanrobotics.sim.motors.Motor;
+import org.vulcanrobotics.sim.motors.MotorType;
+import org.vulcanrobotics.sim.motors.NeverestOrbital20;
+import org.vulcanrobotics.sim.motors.ZeroPowerBehavior;
 
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+//TODO calculate force from motor zero power behavior
 public abstract class RobotModel {
 
     protected volatile Pose robotPose = new Pose();
@@ -11,10 +18,61 @@ public abstract class RobotModel {
 
     protected double wheelRadius;
     protected double robotWeight;
-    protected Motor[] motors;
+
+    //this is private now so that all the physics engine stuff cant be overwritten by a subclass
+    //might be able to use singleton to instantiate this stuff
+    private final Motor[] motors;
+    private final MotorType motorProperties;
+    private final double maxWheelAccel;
+
+    public RobotModel(double wheelRadius, double robotWeight, Motor[] motors) {
+        this.wheelRadius = wheelRadius;
+        this.robotWeight = robotWeight;
+        this.motors = motors;
+        motorProperties = motors[0].getMotorProperties();
+        maxWheelAccel = calculateMaxWheelAccel();
+
+    }
 
     public abstract void update(double... powers) throws Exception;
 
+    //this is where the physics stuff comes in (resistive forces, maximum acceleration, etc)
+    protected void setTargetSpeed(int motorId, double targetSpeed) {
+        double currentSpeed = motors[motorId].getSpeed();
+        double outputSpeed;
+        if(targetSpeed != 0 || motors[motorId].getZeroPowerBehavior() == ZeroPowerBehavior.BRAKE) {
+            double targetAccel = targetSpeed - currentSpeed;
+            double targetOutputAccel = Math.abs(targetAccel) > (maxWheelAccel * loopTime) ? (maxWheelAccel * Math.signum(targetAccel) * loopTime) : targetAccel;
+            double resistiveWheelAccel = robotWeight * motorProperties.resistiveCoeff() * -1.0 * currentSpeed;
+            outputSpeed = currentSpeed + targetOutputAccel + resistiveWheelAccel;
+        } else {
+            double targetOutputAccel = maxWheelAccel * (motorProperties.backDriveTorque() / motorProperties.maxTorque()) * loopTime * -1.0;
+            outputSpeed = currentSpeed + targetOutputAccel;
+        }
+        motors[motorId].speed(outputSpeed);
+    }
+
+    private double calculateMaxWheelAccel() {
+        //unit conversion
+        double wheelRadMeters = wheelRadius * 0.0254;
+        double robotMassKg = robotWeight / 2.205;
+        double maxSpeedRad = motorProperties.maxRPM() * ((2.0 * Math.PI) / 60.0);
+        double maxTorqueNM = motorProperties.maxTorque() * 0.113;
+
+        //max accel in radians
+        double maxWheelAccelRad = (4.0 * maxTorqueNM) / (robotMassKg * Math.pow(wheelRadMeters, 2));
+
+        //max accel in motor percentage
+        return maxWheelAccelRad / maxSpeedRad;
+    }
+
+    protected double[] getWheelVelocities() {
+        double[] motorPowers = new double[motors.length];
+        for (int i = 0; i < motorPowers.length; i++) {
+            motorPowers[i] = motors[i].getSpeed();
+        }
+        return motorPowers;
+    }
 
     //getter and setter for robotPose
     public Pose getRobotPose() {
@@ -24,4 +82,7 @@ public abstract class RobotModel {
         robotPose = new Pose(newPose);
     }
 
+    protected void setRobotPoseVelocity(Pose velocity) {
+        robotPoseVelocity = new Pose(velocity);
+    }
 }
--- src/main/java/org/vulcanrobotics/sim/drivetrains/Mecanum.java
+++ src/main/java/org/vulcanrobotics/sim/drivetrains/Mecanum.java
@@ -15,23 +15,14 @@ public class Mecanum extends RobotModel {
     private final RealMatrix velocityMatrix;
     private final RealMatrix wheelVelocityMatrix;
 
-    private final double maxWheelAccel;
-    private double robotWeight;
-
-    private final Motor[] motors;
-
     public Mecanum(double wheelBaseY, double wheelBaseX, double wheelRadius, double robotWeight, Motor[] motors) {
+        super(wheelRadius, robotWeight, motors);
         wheelVelocityMatrix = constructMecanumWheelVelocityMatrix(wheelBaseY, wheelBaseX, wheelRadius);
         velocityMatrix = constructMecanumVelocityMatrix(wheelBaseY, wheelBaseX, wheelRadius);
         if(motors.length != 4) {
             //TODO error handler of some sort with UI to get rid of console error logs like this
             System.out.println("mecanum needs 4 motors in its configuration");
         }
-        this.motors = motors;
-        this.robotWeight = robotWeight;
-        maxWheelAccel = calculateMaxWheelAccel(wheelRadius, robotWeight, motors[0].getMotorProperties());
-        this.wheelRadius = wheelRadius;
-        System.out.println(calculateMaxFrictionAccelRad());
     }
 
     @Override
@@ -39,52 +30,10 @@ public class Mecanum extends RobotModel {
         if(powers.length != 4) {
             throw new Exception("mecanum requires 4 motor powers");
         }
-        //setup target velocities
-        //might be able to make these local variables depending on how this goes
-        Pose targetVelocity = calculateRobotVelocity(MatrixUtils.createColumnRealMatrix(powers));
 
-        double[] currentWheelVelocities = new double[] {
-                motors[0].getSpeed(),
-                motors[1].getSpeed(),
-                motors[2].getSpeed(),
-                motors[3].getSpeed()
-        };
+        //give motors their target velocity, let motor physics API do the rest?
+        //maybe let RobotModel handle physics. Motor objects should just be for motor intrinsics
 
-        for(int i = 0; i < 4; i++) {
-            double targetAccel = (powers[i] - currentWheelVelocities[i]);
-            double allowedWheelAccel = Math.abs(targetAccel) > (maxWheelAccel * loopTime) ? (maxWheelAccel * Math.signum(targetAccel) * loopTime) : targetAccel;
-            double resistiveWheelAccel = robotWeight * motors[i].getMotorProperties().backDriveTorque() * currentWheelVelocities[i] * -1.0;
-            double wheelForceOutput = allowedWheelAccel + resistiveWheelAccel;
-            currentWheelVelocities[i] += wheelForceOutput;
-            motors[i].speed(currentWheelVelocities[i]);
-        }
-
-        Pose velocityUpdate = calculateRobotVelocity(MatrixUtils.createColumnRealMatrix(currentWheelVelocities).scalarMultiply(loopTime * (motors[0].getMotorProperties().maxRPM()) * (2.0 * Math.PI / 60.0)));
-        setRobotPose(robotPose.plus(velocityUpdate));
-        System.out.println(velocityUpdate);
-    }
-
-    private double calculateMaxWheelAccel(double wheelRadius, double robotWeight, MotorType motor) {
-        //unit conversion
-        double wheelRadMeters = wheelRadius * 0.0254;
-        System.out.println(wheelRadMeters);
-        double robotMassKg = robotWeight / 2.205;
-        double maxSpeedRad = motor.maxRPM() * ((2.0 * Math.PI) / 60.0);
-        double maxTorqueNM = motor.maxTorque() * 0.113;
-
-        //max accel in radians
-        double maxWheelAccelRad = (4.0 * maxTorqueNM) / (robotMassKg * Math.pow(wheelRadMeters, 2));
-
-        //max accel in motor percentage
-        return maxWheelAccelRad / maxSpeedRad;
-
-    }
-
-    private double calculateMaxFrictionAccelRad() {
-        double wheelRadMeters = wheelRadius * 0.0254;
-        double frictionCoefficient = 0.02;
-
-        return (frictionCoefficient * 9.81) / (wheelRadMeters);
     }
 
     public Pose calculateRobotVelocity(RealMatrix wheelVelocities) {
