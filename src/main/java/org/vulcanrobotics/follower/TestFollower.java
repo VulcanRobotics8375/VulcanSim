d470c8df10afce78c3a641788f2c066f8524dfe2
Nico Paoli <39326876+nicobonik@users.noreply.github.com>
2022-01-23 23:44:59 -0800

    code works pogchamp

--- src/main/java/org/vulcanrobotics/follower/TestFollower.java
+++ src/main/java/org/vulcanrobotics/follower/TestFollower.java
@@ -1,18 +1,30 @@
 package org.vulcanrobotics.follower;
 
+import org.apache.commons.math3.linear.MatrixUtils;
 import org.vulcanrobotics.math.geometry.Pose;
 import org.vulcanrobotics.path.Path;
 import org.vulcanrobotics.sim.RobotModel;
+import org.vulcanrobotics.sim.drivetrains.Mecanum;
 
 public class TestFollower extends Follower{
 
-    public TestFollower(Path path, RobotModel model) {
+    Mecanum model;
+
+    public TestFollower(Path path, Mecanum model) {
         super(path, model);
+        this.model = model;
     }
 
     public void run() throws Exception {
         Pose robotPose = model.getRobotPose();
-        model.update(1.0, 1.0, 1.0, 1.0);
+
+        Pose targetPose = new Pose(80.0, 80.0, 0.0);
+        double angleToPoint = Math.atan2(targetPose.y - robotPose.y, targetPose.x - robotPose.x);
+        Pose velocityOut = new Pose(Math.cos(angleToPoint), Math.sin(angleToPoint), 0.0);
+
+        double[] outputWheelVelocities = model.calculateWheelVelocities(MatrixUtils.createColumnRealMatrix(new double[] {velocityOut.x, velocityOut.y, velocityOut.heading}));
+
+        model.update(outputWheelVelocities);
     }
 
 }
